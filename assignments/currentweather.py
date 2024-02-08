d903924b975bcc8b412a139e988e29abb57c4035
Francesco <G00425679@atu.ie>
2024-02-08 18:45:06 +0000

    working on the first assignment

---
+++ assignments/currentweather.py
@@ -0,0 +1,5 @@
+# Using Using the URL: https://api.open-meteo.com/v1/forecast?latitude=53.82&longitude=-9.5&current=temperature_2m
+# Write a program that will print out the current temperature on the console (and only the temperature)
+# Look at the documentation (below) and print out the current wind direction (10m) as well.
+# https://open-meteo.com/
+
